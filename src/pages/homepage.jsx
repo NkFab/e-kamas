e4eb9b9b94b4ea05618d4107edbe6ec94ad8cdfc
nkfab <fabjeezy@gmail.com>
2020-09-16 14:34:27 +0200

    Add base menu

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,12 +1,86 @@
 import React from "react";
 
 const HomePage = () => (
-  <div id="hompage">
-    <div id="directory-menu">
-      <div id="menu-item">
-        <div id="content">
-          <h1 id="title">HATS</h1>
-          <span id="subtitle">SHOP NOW</span>
+  <div id="hompage" className="flex flex-col items-center py-5 p-20">
+    <div id="directory-menu" className="w-full flex flex-wrap justify-between">
+      <div
+        id="menu-item"
+        className=" min-w-3 h-56 flex flex-auto items-center justify-center border border-black mt-0 mx-2 mb-4 first:mr-2 last:ml-2"
+      >
+        <div
+          id="content"
+          className="h-24 py-0 px-6 flex flex-col items-center justify-center border border-black text-center"
+        >
+          <h1 id="title" className="font-bold mb-1 text-xl">
+            HATS
+          </h1>
+          <span id="subtitle" className=" font-light text-base">
+            SHOP NOW
+          </span>
+        </div>
+      </div>
+      <div
+        id="menu-item"
+        className=" min-w-3 h-56 flex flex-auto items-center justify-center border border-black mt-0 mx-2 mb-4 first:mr-2 last:ml-2"
+      >
+        <div
+          id="content"
+          className="h-24 py-0 px-6 flex flex-col items-center justify-center border border-black text-center"
+        >
+          <h1 id="title" className="font-bold mb-1 text-xl">
+            HATS
+          </h1>
+          <span id="subtitle" className=" font-light text-base">
+            SHOP NOW
+          </span>
+        </div>
+      </div>
+      <div
+        id="menu-item"
+        className=" min-w-3 h-56 flex flex-auto items-center justify-center border border-black mt-0 mx-2 mb-4 first:mr-2 last:ml-2"
+      >
+        <div
+          id="content"
+          className="h-24 py-0 px-6 flex flex-col items-center justify-center border border-black text-center"
+        >
+          <h1 id="title" className="font-bold mb-1 text-xl">
+            HATS
+          </h1>
+          <span id="subtitle" className=" font-light text-base">
+            SHOP NOW
+          </span>
+        </div>
+      </div>
+      <div
+        id="menu-item"
+        className=" min-w-3 h-56 flex flex-auto items-center justify-center border border-black mt-0 mx-2 mb-4 first:mr-2 last:ml-2"
+      >
+        <div
+          id="content"
+          className="h-24 py-0 px-6 flex flex-col items-center justify-center border border-black text-center"
+        >
+          <h1 id="title" className="font-bold mb-1 text-xl">
+            HATS
+          </h1>
+          <span id="subtitle" className=" font-light text-base">
+            SHOP NOW
+          </span>
+        </div>
+      </div>
+      <div
+        id="menu-item"
+        className=" min-w-3 h-56 flex flex-auto items-center justify-center border border-black mt-0 mx-2 mb-4 first:mr-2 last:ml-2"
+      >
+        <div
+          id="content"
+          className="h-24 py-0 px-6 flex flex-col items-center justify-center border border-black text-center"
+        >
+          <h1 id="title" className="font-bold mb-1 text-xl">
+            HATS
+          </h1>
+          <span id="subtitle" className=" font-light text-base">
+            SHOP NOW
+          </span>
         </div>
       </div>
     </div>
